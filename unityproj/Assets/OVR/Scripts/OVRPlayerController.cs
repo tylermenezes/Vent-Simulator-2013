185e2e829e6050da10bc87eefb07dac254df076b
Zachary Wiedmann <zaqthefreshman@gmail.com>
2013-08-10 19:25:07 -0700

    treesinwater

--- unityproj/Assets/OVR/Scripts/OVRPlayerController.cs
+++ unityproj/Assets/OVR/Scripts/OVRPlayerController.cs
@@ -220,8 +220,8 @@ public class OVRPlayerController : OVRComponent
 			MoveScale = 0.70710678f;
 			
 		// No positional movement if we are in the air
-		if (!Controller.isGrounded)	
-			MoveScale = 0.0f;
+		//if (!Controller.isGrounded)	
+		//	MoveScale = 0.0f;
 			
 		MoveScale *= DeltaTime;
 			
